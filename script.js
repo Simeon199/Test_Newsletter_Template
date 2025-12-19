ba0c7e918273e1be0abe902b054c47783a90db8b
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-12-19 10:23:35 +0100

    script.js file removed

--- script.js
+++
@@ -1,6 +0,0 @@
-/* Opens the registration form in a separate tab to sign in for the weekly newsletter */
-
-function openRegisterForm(){
-   let url = "https://seu2.cleverreach.com/f/436994-421346/";
-   window.open(url, '_blank');
-}
